728237ec9b77467e3e1877a888fbcc7dc62b983a
anneloesLPRD <contact@anneloesouwehand.com>
2016-03-05 13:27:46 +0100

    My Django Girls app, first commit

---
+++ myvenv1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anneloesouwehand/Desktop/CODE_REPOSITORY/DJANGO_GIRLS/myvenv1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
